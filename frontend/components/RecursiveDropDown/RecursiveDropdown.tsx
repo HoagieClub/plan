eb0ae2190202e064085f9692cd798235e5cd2a1c
George Chiriac <chiriac.gege@gmail.com>
2024-07-28 22:38:02 -0400

    Fixed width and height for settled / unsettled courses + icon

--- frontend/components/RecursiveDropDown/RecursiveDropdown.tsx
+++ frontend/components/RecursiveDropDown/RecursiveDropdown.tsx
@@ -1,11 +1,14 @@
 import { FC, useEffect, useState, useCallback } from 'react';
 
+import { cornersOfRectangle } from '@dnd-kit/core/dist/utilities/algorithms/helpers';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';import { Button as JoyButton } from '@mui/joy';
+import PriorityHigh from '@mui/icons-material/PriorityHigh';
+import { Button as JoyButton } from '@mui/joy';
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -19,7 +22,6 @@ import LoadingComponent from '../LoadingComponent';
 import SettingsModal from '../Modal';
 
 import styles from '../InfoComponent/InfoComponent.module.scss';
-import { cornersOfRectangle } from '@dnd-kit/core/dist/utilities/algorithms/helpers';
 
 interface Dictionary {
   [key: string]: any;
@@ -81,18 +83,7 @@ const SatisfactionStatus: FC<SatisfactionStatusProps> = ({
         </>
       );
     }
-    if (settled + unsettled >= minNeeded) {
-      return (
-        <>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <span style={{ fontWeight: 450, color: 'red' }}>
-              {count}/{minNeeded}
-            </span>
-            <ErrorOutlineIcon style={{ color: 'orange', marginLeft: '10px' }} />
-          </div>
-        </>
-      );
-    }
+
     return (
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <span style={{ fontWeight: 450, color: 'red' }}>
@@ -100,31 +91,21 @@ const SatisfactionStatus: FC<SatisfactionStatusProps> = ({
         </span>
         <HighlightOffIcon style={{ color: 'red', marginLeft: '10px' }} />
       </div>
-    )
+    );
   }
-
 
   if (satisfied === 'True') {
-    return (
-      <CheckCircleOutlineIcon style={{ color: 'green', marginLeft: '10px' }} />
-    );
+    return <CheckCircleOutlineIcon style={{ color: 'green', marginLeft: '10px' }} />;
   }
-  if (settled + unsettled >= minNeeded) {
-    return (
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <span style={{ fontWeight: 450, color: 'red' }}>
-          {count}/{minNeeded}
-        </span>
-        <ErrorOutlineIcon style={{ color: 'orange', marginLeft: '10px' }} />
-      </div>
-    );
-  }
-  return (<div style={{ display: 'flex', alignItems: 'center' }}>
-    <span style={{ fontWeight: 450, color: 'red' }}>
-      {count}/{minNeeded}
-    </span>
-    <HighlightOffIcon style={{ color: 'red', marginLeft: '10px' }} />
-  </div>);
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <span style={{ fontWeight: 450, color: 'red' }}>
+        {count}/{minNeeded}
+      </span>
+      <HighlightOffIcon style={{ color: 'red', marginLeft: '10px' }} />
+    </div>
+  );
 };
 
 // Dropdown component with refined styling
@@ -404,121 +385,136 @@ const Dropdown: FC<DropdownProps> = ({ data, csrfToken, checkRequirements }) => 
   };
 
   const renderContent = (data: Dictionary) => {
-    return Object.entries(data).map(([key, value]) => {
-      if (
-        key === 'req_id' ||
-        key === 'satisfied' ||
-        key === 'manually_satisfied' ||
-        key === 'count' ||
-        key === 'min_needed' ||
-        key === 'max_counted'
-      ) {
-        return null;
-      }
-      const isArray = Array.isArray(value);
-      if (isArray) {
-        if (key === 'settled') {
-          // Render as disabled buttons
-          return value[0].map((item, index) => (
-            <Button
-              key={index}
-              variant='contained'
-              disabled={!item['manually_settled']}
-              style={{
-                margin: '5px',
-                backgroundColor: '#f7f7f7',
-                color: '#000',
-              }}
-              onClick={() => handleClick(item['crosslistings'], value[1])}
+    return (
+      <div className='settledContainer'>
+        {Object.entries(data).map(([key, value]) => {
+          if (
+            key === 'req_id' ||
+            key === 'satisfied' ||
+            key === 'manually_satisfied' ||
+            key === 'count' ||
+            key === 'min_needed' ||
+            key === 'max_counted'
+          ) {
+            return null;
+          }
+          const isArray = Array.isArray(value);
+          if (isArray) {
+            if (key === 'settled') {
+              // Render as disabled buttons
+              return value[0].map((item, index) => (
+                <Button
+                  key={index}
+                  variant='contained'
+                  disabled={!item['manually_settled']}
+                  style={{
+                    display: 'inline-block',
+                    width: '45%',
+                    margin: '5px',
+                    backgroundColor: '#f7f7f7',
+                    color: '#000',
+                    whiteSpace: 'nowrap',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                  }}
+                  onClick={() => handleClick(item['crosslistings'], value[1])}
+                >
+                  {item['code']}
+                </Button>
+              ));
+            } else if (key === 'unsettled') {
+              // Render as normal buttons
+              return value[0].map((item, index) => (
+                <Button
+                  key={index}
+                  variant='contained'
+                  style={{
+                    display: 'inline-block',
+                    width: '45%',
+                    margin: '5px',
+                    backgroundColor: '#f7f7f7',
+                    color: '#000',
+                    whiteSpace: 'nowrap',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    opacity: 0.5,
+                  }}
+                  onClick={() => handleClick(item['crosslistings'], value[1])}
+                  startIcon={<ErrorOutlineIcon style={{ color: 'orange' }} />}
+                >
+                  {item['code']}
+                </Button>
+              ));
+            }
+          }
+          const isObject = typeof value === 'object' && value !== null && !Array.isArray(value);
+          const isRestrictions = key === 'Restrictions';
+          const satisfactionElement =
+            isObject && 'satisfied' in value ? (
+              <SatisfactionStatus
+                satisfied={value.satisfied}
+                manuallySatisfied={value.manually_satisfied}
+                count={value.count}
+                minNeeded={value.min_needed}
+                maxCounted={value.max_counted}
+                settled={value.settled ? value.settled[0].length : 0}
+                unsettled={value.unsettled ? value.unsettled[0].length : 0}
+                isRestrictions={isRestrictions}
+              />
+            ) : null;
+          const subItems = isObject ? { ...value, satisfied: undefined } : value;
+          let settledEmpty = false;
+          let unsettledEmpty = false;
+
+          if (Object.prototype.hasOwnProperty.call(value, 'settled')) {
+            if (Array.isArray(value['settled']) && value['settled'].length > 0) {
+              settledEmpty = Array.isArray(value['settled'][0]) && value['settled'][0].length === 0;
+            }
+          }
+          if (Object.prototype.hasOwnProperty.call(value, 'unsettled')) {
+            if (Array.isArray(value['unsettled']) && value['unsettled'].length > 0) {
+              unsettledEmpty =
+                Array.isArray(value['unsettled'][0]) && value['unsettled'][0].length === 0;
+            }
+          }
+
+          const hasItems = settledEmpty && unsettledEmpty;
+          const hasNestedItems = isObject && Object.keys(subItems).length > 0;
+
+          // Style adjustments for accordion components
+          return (
+            <Accordion
+              key={key}
+              style={{ margin: '0', boxShadow: 'none', borderBottom: '1px solid #e0e0e0' }}
+              expanded={!expanded.has(key)}
+              onChange={(event) => handleChange(event, key)} // TODO: disable propagation in modals
             >
-              {item['code']}
-            </Button>
-          ));
-        } else if (key === 'unsettled') {
-          // Render as normal buttons
-          return value[0].map((item, index) => (
-            <Button
-              key={index}
-              variant='contained'
-              style={{
-                margin: '5px',
-                backgroundColor: '#f7f7f7',
-                color: '#000',
-                opacity: 0.5,
-              }}
-              onClick={() => handleClick(item['crosslistings'], value[1])}
-            >
-              {item['code']}
-            </Button>
-          ));
-        }
-      }
-      const isObject = typeof value === 'object' && value !== null && !Array.isArray(value);
-      const isRestrictions = key === 'Restrictions';
-      const satisfactionElement =
-        isObject && 'satisfied' in value ? (
-          <SatisfactionStatus
-            satisfied={value.satisfied}
-            manuallySatisfied={value.manually_satisfied}
-            count={value.count}
-            minNeeded={value.min_needed}
-            maxCounted={value.max_counted}
-            settled={value.settled ? value.settled[0].length : 0}
-            unsettled={value.unsettled ? value.unsettled[0].length : 0}
-            isRestrictions={isRestrictions}
-          />
-        ) : null;
-      const subItems = isObject ? { ...value, satisfied: undefined } : value;
-      let settledEmpty = false;
-      let unsettledEmpty = false;
-
-      if (Object.prototype.hasOwnProperty.call(value, 'settled')) {
-        if (Array.isArray(value['settled']) && value['settled'].length > 0) {
-          settledEmpty = Array.isArray(value['settled'][0]) && value['settled'][0].length === 0;
-        }
-      }
-      if (Object.prototype.hasOwnProperty.call(value, 'unsettled')) {
-        if (Array.isArray(value['unsettled']) && value['unsettled'].length > 0) {
-          unsettledEmpty =
-            Array.isArray(value['unsettled'][0]) && value['unsettled'][0].length === 0;
-        }
-      }
-
-      const hasItems = settledEmpty && unsettledEmpty;
-      const hasNestedItems = isObject && Object.keys(subItems).length > 0;
-
-      // Style adjustments for accordion components
-      return (
-        <Accordion
-          key={key}
-          style={{ margin: '0', boxShadow: 'none', borderBottom: '1px solid #e0e0e0' }}
-          expanded={!expanded.has(key)}
-          onChange={(event) => handleChange(event, key)} // TODO: disable propagation in modals
-        >
-          <AccordionSummary
-            expandIcon={hasNestedItems && !hasItems ? <ExpandMoreIcon /> : null}
-            aria-controls={`${key}-content`}
-            id={`${key}-header`}
-            style={{ backgroundColor: '#f6f6f6' }} // subtle background color
-          >
-            <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-              <div
-                className={classNames(styles.Action)}
-                onClick={(event) => handleExplanationClick(event, data[key]['req_id'])}
+              <AccordionSummary
+                expandIcon={hasNestedItems && !hasItems ? <ExpandMoreIcon /> : null}
+                aria-controls={`${key}-content`}
+                id={`${key}-header`}
+                style={{ backgroundColor: '#f6f6f6' }} // subtle background color
               >
-                <Typography style={{ fontWeight: 500 }}>{key}</Typography>
-              </div>
-              {satisfactionElement}
-            </div>
-          </AccordionSummary>
-          {!hasItems && (
-            <AccordionDetails>
-              {hasNestedItems ? renderContent(subItems) : <Typography>{value}</Typography>}
-            </AccordionDetails>
-          )}
-        </Accordion>
-      );
-    });
+                <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+                  <div
+                    className={classNames(styles.Action)}
+                    onClick={(event) => handleExplanationClick(event, data[key]['req_id'])}
+                  >
+                    <Typography style={{ fontWeight: 500 }}>{key}</Typography>
+                  </div>
+                  {satisfactionElement}
+                </div>
+              </AccordionSummary>
+              {!hasItems && (
+                <AccordionDetails>
+                  {hasNestedItems ? renderContent(subItems) : <Typography>{value}</Typography>}
+                </AccordionDetails>
+              )}
+            </Accordion>
+          );
+        })}
+      </div>
+    );
   };
 
   return (
