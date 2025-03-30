c58fcde66d10a77f6efdf69b756f4ef73c66f3a4
issacli-0821 <tl1719@princeton.edu>
2025-03-29 20:20:34 -0400

    Changed background color of widget to match with other containers

--- frontend/components/Widgets/Widget.tsx
+++ frontend/components/Widgets/Widget.tsx
@@ -13,13 +13,13 @@ interface ButtonWidgetProps {
 
 export const ButtonWidget: FC<ButtonWidgetProps> = ({ href, text, icon }) => {
 	return href ? (
-		<Button variant='outline' className='w-full hover:bg-gray-200'>
+		<Button variant='outline' className='bg-slate-100 w-full hover:bg-gray-200'>
 			<Link href={href} className='flex items-center justify-center gap-2'>
 				{icon} {text}
 			</Link>
 		</Button>
 	) : (
-		<Button variant='outline' className='w-full hover:bg-gray-200'>
+		<Button variant='outline' className='bg-slate-100 w-full hover:bg-gray-200'>
 			<div className='flex items-center justify-center gap-2'>
 				{icon}
 				{text}
