f7714f8580df87901e9b231a877b901c4048294c
issacli-0821 <tl1719@princeton.edu>
2025-11-04 16:04:18 -0500

    Modified canceled location logic

--- backend/hoagieplan/api/calendar/ical_generator.py
+++ backend/hoagieplan/api/calendar/ical_generator.py
@@ -124,8 +124,9 @@ def generate_class_ical(cal: Calendar, calendar_event: Dict, semester_code: str)
     location = section.get("class_meetings")[0].get("building_name")
     room = section.get("class_meetings")[0].get("room")
     full_location = location + " " + room
-    if full_location and full_location != "Canceled":
+    if full_location and "Canceled" not in full_location:
         event.add("location", full_location)
+        print(full_location)
     else:
         print("No location")
 
