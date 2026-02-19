7088a19a8aa87e5004d0e404c14cbb5c35a4c0a9
anub mukher <anubmukher@dynamic-oit-ip4-wifirestricted03-10-49-227-152.princeton.edu>
2026-02-18 22:07:09 -0500

    donezo i added my name and github

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -158,6 +158,11 @@ const contributors: HoagieChef[] = [
 		graduationYear: '2028',
 		link: 'https://www.linkedin.com/in/linsey-zhong-686a72309/',
 	},
+	{
+		name: 'Eric Chen',
+		graduationYear: '2029',
+		link: 'https://www.linkedin.com/in/eric--c/',
+	},
 ];
 
 const LinkedInIcon = (props: SVGProps<SVGSVGElement>) => (
