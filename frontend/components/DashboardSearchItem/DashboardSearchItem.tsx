3d7fe6abb7fe875160ed47ed11ae0fbddbed0fb1
Patrick Fu <fu.patrick.0@gmail.com>
2026-02-25 22:19:45 -0500

    added the tag images for spring fall

--- frontend/components/DashboardSearchItem/DashboardSearchItem.tsx
+++ frontend/components/DashboardSearchItem/DashboardSearchItem.tsx
@@ -3,6 +3,8 @@ import type { FC, ReactNode } from 'react';
 import type { Course } from '@/types';
 import { getRatingBackground } from '@/utils/ratingColors';
 
+import Image from 'next/image'; 
+
 import styles from './DashboardSearchItem.module.css';
 
 interface DashboardSearchItemProps {
@@ -34,6 +36,14 @@ export const DashboardSearchItem: FC<DashboardSearchItemProps> = ({
 						>
 							{course.quality_of_course.toFixed(2)}
 						</div>
+					)}
+					{course.guid && (
+						<Image 
+							src={Number(course.guid[3]) === 2 ? '/fall tag.svg' : '/spring tag.svg'}
+							alt={Number(course.guid[3]) === 2 ? 'Fall' : 'Spring'}
+							width={Number(course.guid[3]) === 2 ? 56 : 72}
+							height={23}
+						/>
 					)}
 				</div>
 				{children && <div className={styles.chipContainer}>{children}</div>}
