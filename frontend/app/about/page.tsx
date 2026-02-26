93e7838d6facb07fc02b48d15db804a9fcc922c2
chap | mouzai <151426364+chap2457@users.noreply.github.com>
2026-02-25 22:16:22 -0500

    donezo i added my name and github (#421)

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -73,6 +73,10 @@ const contributors: HoagieChef[] = [
 		graduationYear: '2027',
 		link: 'https://www.linkedin.com/in/lucy-c1/',
 	},
+  {
+		name: 'Eric Chen',
+		graduationYear: '2029',
+		link: 'https://www.linkedin.com/in/eric--c/',
 	{
 		name: 'Grace Ding',
 		graduationYear: '2029',
@@ -158,7 +162,7 @@ const contributors: HoagieChef[] = [
 		graduationYear: '2028',
 		link: 'https://www.linkedin.com/in/linsey-zhong-686a72309/',
 	},
-	{
+  {
 		name: 'Ruoming Shen',
 		graduationYear: '2027',
 		link: 'https://www.linkedin.com/in/ruomingshen/',
