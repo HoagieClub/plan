9aa22d8dab66b1c7c4c90cac659730837097cde2
issacli-0821 <tl1719@princeton.edu>
2026-03-24 16:32:03 -0400

    Update CourseSerializer to serialize instructors

--- backend/hoagieplan/serializers.py
+++ backend/hoagieplan/serializers.py
@@ -42,6 +42,7 @@ class CourseSerializer(serializers.ModelSerializer):
     # Nested SectionSerializer to include section details in the course data
     sections = SectionSerializer(many=True, read_only=True)
     department_code = serializers.CharField(source="department.code", read_only=True)
+    instructors = serializers.StringRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Course
@@ -65,6 +66,7 @@ class CourseSerializer(serializers.ModelSerializer):
             "sections",
             "crosslistings",
             "quality_of_course",
+            "instructors",
         )
 
 
