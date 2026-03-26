1bd63c970c3e1c618d833424cc312c744f289ef0
Ruoming Shen <rs3270@princeton.edu>
2026-03-25 20:45:33 -0400

    Create userService.ts to support get_user and update profile operations

---
+++ frontend/services/userService.ts
@@ -0,0 +1,70 @@
+import type { Profile } from '@/types';
+
+import { fetchCsrfToken } from '@/utils/csrf';
+
+// Fetch user profile from the backend
+async function fetchCustomUser(
+    netId: string,
+    firstName: string,
+    lastName: string,
+    email: string
+): Promise<Profile | null> {
+    try {
+        const csrfToken = await fetchCsrfToken();
+
+        const response = await fetch(`/api/hoagie/profile/get_user`, {
+            method: 'POST',
+            headers: {
+                'X-CSRFToken': csrfToken,
+            },
+            body: JSON.stringify({
+                netId,
+                firstName,
+                lastName,
+                email,
+            }),
+        });
+
+        if (!response.ok) {
+            if (response.status === 401) {
+                console.error('User not authenticated.');
+                return null;
+            }
+            if (response.status === 404) {
+                console.error('User profile not found.');
+                return null;
+            }
+            throw new Error(`Error fetching CustomUser: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data as Profile;
+    } catch (error) {
+        console.error('Error fetching CustomUser:', error);
+        return null;
+    }
+}
+
+async function updateUserProfile(
+    updatedProfile: Profile
+): Promise<Profile | null> {
+    try {
+        const csrfToken = await fetchCsrfToken();
+        const response = await fetch(`/api/hoagie/profile/update`, {
+            method: 'POST',
+            headers: {
+                'X-CSRFToken': csrfToken,
+            },
+            body: JSON.stringify(updatedProfile),
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to update profile');
+        }
+        return null
+    } catch (error) {
+        console.error('Error updating profile:', error);
+    }
+}
+
+		
