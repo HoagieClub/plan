7d24e21ecf57bea6c2e8149c2febe1f09ffc62f2
chap2457 <chene2457@gmail.com>
2026-04-01 22:18:16 -0400

    I made the courses pretty when you search them on the 4 year plan. YIPPEE!!! 你食咗飯未呀

--- frontend/components/DashboardSearchItem/DashboardSearchItem.tsx
+++ frontend/components/DashboardSearchItem/DashboardSearchItem.tsx
@@ -1,6 +1,9 @@
 import type { FC, ReactNode } from 'react';
 
 import type { Course } from '@/types';
+import { getAuditColor, getAuditTag } from '@/utils/auditTag';
+import { getDistributionColors } from '@/utils/distributionColors';
+import { getPdfColor, getPdfTag } from '@/utils/pdfTag';
 import { getRatingBackground } from '@/utils/ratingColors';
 
 import styles from './DashboardSearchItem.module.css';
@@ -22,21 +25,40 @@ export const DashboardSearchItem: FC<DashboardSearchItemProps> = ({
 		}
 	};
 
+	const distColor = getDistributionColors(course.distribution_area_short);
+
+	const gradingBasis = course.grading_basis?.['Grading Basis'];
+	const pdfTag = getPdfTag(gradingBasis);
+	const pdfColor = getPdfColor(pdfTag);
+
+	const auditTag = getAuditTag(gradingBasis);
+	const auditColor = getAuditColor(auditTag);
+
 	return (
 		<div className={styles.card} onClick={handleClick}>
 			<div className={styles.content}>
+				{children && <div className={styles.chipContainer}>{children}</div>}
 				<div className={styles.titleRow}>
 					<div className={styles.title}>{course.title}</div>
+
 					{course.quality_of_course && (
 						<div
 							className={styles.rating}
-							style={{ background: getRatingBackground(course.quality_of_course) }}
+							style={{
+								background: getRatingBackground(course.quality_of_course),
+							}}
 						>
 							{course.quality_of_course.toFixed(2)}
 						</div>
 					)}
 				</div>
-				{children && <div className={styles.chipContainer}>{children}</div>}
+				<div className={styles.titleRow} style={{ justifyContent: 'left' }}>
+					<div style={{ color: distColor, paddingRight: '10px' }}>
+						{course.distribution_area_short}
+					</div>
+					<div style={{ color: pdfColor, paddingRight: '10px' }}>{getPdfTag(gradingBasis)}</div>
+					<div style={{ color: auditColor }}>{getAuditTag(gradingBasis)}</div>
+				</div>
 			</div>
 		</div>
 	);
